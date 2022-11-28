f667501867f37d4d09d770229561d166c05b382d
Nishant <nishant.mobcoder@zluri.com>
2022-11-29 00:40:55 +0530

    Palindrome

---
+++ javascript/Mathematics/Palindrome.js
@@ -0,0 +1,18 @@
+function Palindrome(num){
+    let newNum = 0
+    const oldNum = num
+    while(num>0){
+        rem = num%10
+        newNum = newNum * 10 + rem
+        num = Math.round(num/10)
+    }
+    if(newNum == oldNum){
+        return true
+    }else{
+        return false
+    }
+}
+
+
+
+console.log(Palindrome(123))
