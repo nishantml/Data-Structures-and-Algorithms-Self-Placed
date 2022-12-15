d3827e6e26ae0885e98f96d36b633a9d00af34dd
Nishant <nishantbhushan10@gmail.com>
2022-12-15 10:32:42 +0530

    LeaderInAnArray

--- javascript/Array/LeaderInAnArray.js
+++ javascript/Array/LeaderInAnArray.js
@@ -22,3 +22,29 @@ function LeadersInAnArray(arr){
 
 console.log(LeadersInAnArray([16,17,4,3,5,2]))
 console.log(LeadersInAnArray([1,2,3,4,0]))
+
+
+
+/*
+Efficient than Naive
+ */
+
+
+function LeadersInAnArray1(arr){
+    const newA = []
+    let currentLeader = arr[arr.length-1]
+    newA.push(currentLeader)
+    for(let i= arr.length-2;i>=0;i--){
+        if(currentLeader < arr[i]){
+            currentLeader = arr[i]
+            newA.push(arr[i])
+        }
+    }
+    return newA.reverse()
+}
+
+
+
+
+console.log(LeadersInAnArray1([16,17,4,3,5,2]))
+console.log(LeadersInAnArray1([1,2,3,4,0]))
