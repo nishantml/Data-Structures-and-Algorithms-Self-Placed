ff6d309593f4a95a39bbda5d9e39a4467b78b7e0
Nishant <nishantbhushan10@gmail.com>
2022-12-15 10:05:17 +0530

    rotateArray

--- javascript/Array/rotateArray.js
+++ javascript/Array/rotateArray.js
@@ -18,3 +18,30 @@ console.log(rotateArray([1,2,3,4,5],2))
 console.log(rotateArray([2,4,6,8,10,12,14,16,18,20],3))
 
 
+
+
+/*
+ Naive soultion
+*/
+
+function leftRotateAnArrayByOne(arr){
+    const firstELE = arr[0]
+    for(let i=1;i<arr.length;i++){
+        arr[i-1] = arr[i]
+    }
+    arr[arr.length-1] = firstELE
+    return arr
+}
+
+
+function leftRotateArray(arr,d){
+    for(let i = 0;i<d;i++){
+        arr = leftRotateAnArrayByOne(arr)
+    }
+    return arr
+}
+
+console.log(leftRotateArray([1,2,3,4,5],3))
+console.log(leftRotateArray([10,11,12,13,14],2))
+
+
