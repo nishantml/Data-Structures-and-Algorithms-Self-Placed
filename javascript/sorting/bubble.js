a1f1a3e08319f39ffdc698de2520ec8047973ea1
Nishant <nishantbhushan10@gmail.com>
2023-02-23 09:58:05 +0530

    bubble

---
+++ javascript/sorting/bubble.js
@@ -0,0 +1,21 @@
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ */
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+    bubbleSort(arr,n){
+        for(let i = 0;i<arr.length;i++){
+            for(let j = 0;j<arr.length-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    const temp = arr[j+1]
+                    arr[j+1] = arr[j]
+                    arr[j] = temp
+                }
+            }
+        }
+    }
+
+}
