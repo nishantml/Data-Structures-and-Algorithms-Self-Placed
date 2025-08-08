600504cfb7cb75835aac4e6a2aa06e946a444bdf
nishant <nishantbhushan10@gmail.com>
2025-08-08 17:22:40 +0530

    code till 8 aug

---
+++ javacode/src/main/java/Odds.java
@@ -0,0 +1,17 @@
+public class Odds {
+    public static void main(String[] args) {
+//        String num = "39427";
+        String num = "4206";
+        String[] nums = num.split("");
+        int largestOdd = 1;
+        for (int i = 0; i < nums.length; i++) {
+            int nn = Integer.parseInt(nums[i]);
+            if (nn % 2 != 0){
+                if (largestOdd < nn){
+                    largestOdd = nn;
+                }
+            }
+        }
+        System.out.println(" :: "+largestOdd);
+    }
+}
